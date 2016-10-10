18845f5f940cc8f4b1cc69cf323949b2144f367b
Alex Park <alex.park@opternative.com>
2016-10-10 13:29:16 -0500

    Install momentjs and use Rails provided timezone

--- app/assets/webpack/index.js
+++ app/assets/webpack/index.js
@@ -1,3 +1,22 @@
+var moment = require('moment');
+
 export function helloWorld () {
   console.log('hello world', require('./logo.png'));
 }
+
+export function appTime () {
+  if(opt_tz_offset) {
+    return moment().utcOffset(opt_tz_offset/60);
+  } else {
+    return moment();
+  }
+}
+
+export function browserTime () {
+  return moment();
+}
+
+export function m () {
+  return moment;
+}
+
