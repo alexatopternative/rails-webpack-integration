3eba354f8150e7da9d9312e4119449a15244e168
Alex Park <alex.park@opternative.com>
2016-10-10 11:07:15 -0500

    install & reference node libraries

--- app/assets/webpack/index.js
+++ app/assets/webpack/index.js
@@ -1,3 +1,9 @@
+var _ = require('lodash/fp');
+
 export function helloWorld () {
   console.log('hello world', require('./logo.png'));
+}
+
+export function togetherness(x, y) {
+  return _.union(x, y);
 }
